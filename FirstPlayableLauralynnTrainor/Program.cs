eeaa4c7198d77c1823a5b0d755ce5891662f7c09
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-11-14 16:22:30 -0400

    Added map

--- FirstPlayableLauralynnTrainor/Program.cs
+++ FirstPlayableLauralynnTrainor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FirstPlayableLauralynnTrainor
@@ -10,6 +11,85 @@ namespace FirstPlayableLauralynnTrainor
     {
         static void Main(string[] args)
         {
+            DisplayMap(2);
+            Console.ReadKey(true);
         }
+        static void DisplayMap(int s)
+        {
+            //DisplayBorderTop(s);
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+
+                for (int p = 0; p < s; p++)
+                {
+                    //Console.Write(borderVertical);
+                    for (int y = 0; y < map.GetLength(1); y++)
+                    {
+                        for (int z = 0; z < s; z++)
+                        {
+                            if (map[x, y] == '`')
+                            {
+                                ColorChange(ConsoleColor.Black, ConsoleColor.White);
+                                Console.Write("`");
+                            }
+                            else if (map[x, y] == '~')
+                            {
+                                ColorChange(ConsoleColor.White, ConsoleColor.Black);
+                                Console.Write("~");
+                            }
+                            else if (map[x, y] == '>')
+                            {
+                                ColorChange(ConsoleColor.Gray, ConsoleColor.Black);
+                                Console.Write(">");
+                            }
+                            else if (map[x, y] == '!')
+                            {
+                                ColorChange(ConsoleColor.DarkGray, ConsoleColor.White);
+                                Console.Write("!");
+                            }
+                            else if (map[x, y] == '?')
+                            {
+                                ColorChange(ConsoleColor.DarkYellow, ConsoleColor.Black);
+                                Console.Write("?");
+                            }
+                        }
+                    }
+                    ColorChange(ConsoleColor.Black, ConsoleColor.White);
+                    //Console.Write(borderVertical);
+                    Console.Write("\n");
+                }
+
+            }
+            //DisplayBorderBottom(s);
+            Console.WriteLine("DisplayMap(" + s + ")");
+            //DisplayLegend();
+        }
+        static void ColorChange(ConsoleColor background, ConsoleColor foreground)
+        {
+            Console.BackgroundColor = background;
+            Console.ForegroundColor = foreground;
+        }
+        static char[,] map = new char[,] // dimensions defined by following data:
+    {
+            {'`','~','`','~','`','~','`','~','`','~','!','!','?','?','?','?','?','?','?','?','?','?','?','!','?','?','?','?','?','?'},
+            {'~','`','~','`','~','`','~','`','~','`','!','!','?','?','?','?','?','?','?','?','?','?','?','!','?','?','?','?','?','?'},
+            {'`','~','`','~','`','~','`','~','`','~','>','?','?','?','?','?','?','?','?','?','?','?','?','>','?','?','?','?','?','?'},
+            {'!','!','!','!','!','!','!','!','~','`','!','!','?','?','?','?','?','!','?','?','?','?','?','!','!','!','!','!','!','!'},
+            {'?','?','?','?','?','?','!','!','`','~','!','!','?','?','?','?','?','!','?','?','?','?','?','!','!','~','`','~','`','~'},
+            {'?','?','?','?','?','?','!','!','~','`','!','!','?','?','?','?','?','!','?','?','?','?','?','!','!','`','~','`','~','`'},
+            {'?','?','?','?','?','?','!','!','`','~','!','!','?','?','?','?','?','!','!','!','!','!','!','!','!','~','`','~','`','~'},
+            {'!','!','!','!','!','!','!','!','~','`','!','!','!','!','!','!','!','!','~','`','~','`','~','`','~','`','~','`','~','`'},
+            {'`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~'},
+            {'~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`'},
+            {'`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~'},
+            {'~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`','~','`'},
+           };
     }
+    /*legend
+    ` = black
+    ~ = white
+    ! = darkgray
+    ? = darkyellow
+    > = gray
+    */
 }
